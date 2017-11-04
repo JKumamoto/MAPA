af4fa14a9cc761621ebcfda487c88c9aaea9f2b6
Jefferson <jeff.kmt@gmail.com>
2017-11-04 11:39:51 -0200

    First commit

---
+++ src/main/java/br/edu/ufabc/MAPA/MapaApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ufabc.MAPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapaApplication.class, args);
+	}
+}
